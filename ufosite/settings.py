f9f8034ffed2465e0e70ab8b027782236101844d
Luke <luke.peacock@live.com>
2022-06-24 19:29:47 +0100

    all

--- ufosite/settings.py
+++ ufosite/settings.py
@@ -96,15 +96,15 @@ SECRET_KEY = 'django-insecure-ty8$c8=rv6kipt$6(w((h(7&x$k!pycylkjw7zp3^e0=*uv00z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ufosite',
+        'NAME': '',
         'USER': 'postgres',
-        'PASSWORD': 'Peacock',
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
